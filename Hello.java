0d548d38c78fd4df4b5788a643e92e3a0bfc21e7
Salil-chalise <112026205+Salil-chalise@users.noreply.github.com>
2022-08-24 17:06:56 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/************************************************************
+*Hello.Java
+*************************************************************
+*Salil Chalise
+*8.24.2022
+*CMSC-255-001
+*************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("<Salil Chalise>");
+		System.out.println("<Windows x64>");
+		System.out.println("<Hello.Java>");
+		System.out.println("<8/24/2022>");
+		System.out.println("<CMSC-255-001>");
+		System.out.println();
+
+	}
+}	
